0fef4e74ac1c2cf29a4f394c078e6b69fa18ff4b
Alan Latteri <alan@instinctual.la>
2019-12-02 08:38:34 -0800

    Update backburner_tar_demo.sh
    use variables for BB Manager and Group.

--- backburner_tar_demo.sh
+++ backburner_tar_demo.sh
@@ -10,5 +10,7 @@
 #the tar and sort it so it is in numberical order, put that to a human readable file
 
 TARDIR=$1
+BBMANAGER=manager
+BBGROUP=TARNODES
 
-/opt/Autodesk/backburner/cmdjob -manager:manager -group:TarGroup -priority:30 -jobName "`basename $TARDIR` DCDM" -userRights -description "TAR + List file" sh -c "/usr/bin/tar -cvf $TARDIR.tar -C `dirname $TARDIR` `basename $TARDIR` ; tar -tvf $TARDIR.tar | sort > $TARDIR.tar.list"
+/opt/Autodesk/backburner/cmdjob -manager:$BBMANAGER -group:$BBGROUP -priority:30 -jobName "`basename $TARDIR` DCDM" -userRights -description "TAR + List file" sh -c "/usr/bin/tar -cvf $TARDIR.tar -C `dirname $TARDIR` `basename $TARDIR` ; tar -tvf $TARDIR.tar | sort > $TARDIR.tar.list"
